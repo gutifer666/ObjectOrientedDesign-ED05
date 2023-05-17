f57f6b03f2be701c2d99fe97a26d977cd13222b9
gutif <gutifer666@gmail.com>
2023-05-17 10:42:14 +0200

    terminados todos los apartados

---
+++ entorno-ud5/src/main/java/com/mycompany/entorno/ud5/EntornoUd5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.entorno.ud5;
+
+/**
+ *
+ * @author gutif
+ */
+public class EntornoUd5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
